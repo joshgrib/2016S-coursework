1b8f4f59af31cc6e0590952813a74df213e64cda
joshgrib <jgribbon@stevens.edu>
2016-03-03 15:31:20 -0500

    Adding Borowskis graph algorithms

---
+++ cs385/workspace/hw4_waterjug/waterjugpuzzle.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Name        : waterjugpuzzle.cpp
+ * Author      : Josh Gribbon
+ * Date        : 3-9-16
+ * Description : Finds a way to pour water between jugs to get to a certain end situation
+ * Pledge      : I pledge my honor I have abided by the Stevens Honor Code
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * const argv[]){
+    if (argc != 7) {
+        cerr << "Usage: ./waterjugpuzzle <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
+        return 1;
+    }
+    return 0;
+}
+
+
+
